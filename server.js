d690d0491ae9a94c25809973c4297f077b2d5b43
Boris <swarfey@protonmail.com>
2025-02-27 18:03:11 +0100

    get port from env

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(cors())
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -66,5 +66,5 @@ app.get('/transactions', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
